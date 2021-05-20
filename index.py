ede9554ec24b337f2314666ea9e21492a64aa6c0
Tatyana Rudkovskaya <ta.rudkovskaya@gmail.com>
2021-05-20 21:46:58 +0200

    First Python methods

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
 print(5+2)  
+print(20)
+print("Hello World")
+name = "Tatyana"
+print (name)
+print ("Welcom "+name )
+user=["Tatyana", "Tommy", "Yakov"] #arrays
+print(user)
+print(user[1:]) # array slicing
